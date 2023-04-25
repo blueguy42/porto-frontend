f6ae0441ee9c0fa833a003822059d8985cf43fd1
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-26 00:43:41 +0700

    about page essential done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,49 +1,80 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import MuiMarkdown from 'mui-markdown';
 
 import { Typography, Box, Grow, Grid, IconButton, Link, Tooltip, Zoom  } from "@mui/material";
 import { Mail, LinkedIn, GitHub, Instagram } from '@mui/icons-material';
 import { BreakpointName } from "../utils";
 import { motion } from "framer-motion";
+import { Loader } from '../components';
+
+import axios from "axios";
 
 const About = () => {
     const breakpoint = BreakpointName();
-    console.log(breakpoint);
+    
+    const [ slogan, setSlogan ] = useState(-1);
+    const [ description, setDescription ] = useState(-1);
+    const [ links, setLinks ] = useState(-1);
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_BASE_URL + 'about/getSlogan')
+        .then(response => {
+            setSlogan((response.data.slogan).split(/\r\n|\r|\n/));
+        }).catch((error) => {
+            console.error(error);
+        });
+
+        axios.get(process.env.REACT_APP_BASE_URL + 'about/getDescription')
+        .then(response => {
+            setDescription(response.data.description);
+        }
+        ).catch((error) => {
+            console.error(error);
+        });
+
+        axios.get(process.env.REACT_APP_BASE_URL + 'about/getLink')
+        .then(response => {
+            const email = response.data.email;
+            const linkedin = response.data.linkedin;
+            const github = response.data.github;
+            const instagram = response.data.instagram;
+
+            setLinks({email, linkedin, github, instagram});
+        }).catch((error) => {
+            console.error(error);
+        });
+    },[]);
 
     return (
         <>
-            <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={{width: "100%"}}>
+            <motion.div key={`${slogan} ${description} ${links}`} exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} style={{width: "100%"}}>
+                { slogan !== -1 && description !== -1 && links !== -1 ?
+                <>
                 <Grow in={true} timeout={1000}>
                     <Grid container textAlign="center" alignContent="center" alignItems="center" px={{xs: 3, sm: 4, md: 0}} spacing={{ xs: 3, sm: 3, md: 12 }}>
                         <Grid item xs={12} sm={12} md={6} lg={8} order={{ xs: 2, sm: 2, md: 1 }}>
-                        <Typography fontWeight="bold" textAlign={ breakpoint === 'xs' || breakpoint === 'sm' ? 'center': 'left'}
-                                variant={
-                                    breakpoint === 'xs' ? 'h6' :
-                                    breakpoint === 'sm' ? 'h4' :
-                                    breakpoint === 'md' ? 'h5' :
-                                    breakpoint === 'lg' ? 'h3' :
+                            <Box>
+                                <Typography fontWeight="bold" textAlign={ breakpoint === 'xs' || breakpoint === 'sm' ? 'center': 'left'}
+                                    variant={
+                                        breakpoint === 'xs' ? 'h6' :
+                                        breakpoint === 'sm' ? 'h4' :
+                                        breakpoint === 'md' ? 'h5' :
+                                        breakpoint === 'lg' ? 'h3' :
                                     'h2'} mb={4}>
-                                    Code with purpose,<br/>
-                                <span className="second-heading">engineer with precision.</span>
-                            </Typography>
-                            <Typography textAlign='left'
-                                variant={
-                                    breakpoint === 'xs' ? 'body2' :
-                                    breakpoint === 'sm' ? 'body2' :
-                                    breakpoint === 'md' ? 'body2' :
-                                    breakpoint === 'lg' ? 'body1' :
-                                    'body1'} mb={2}>
-                                    Hi, I'm Ahmad Alfani Handoyo, but you can call me Afan. I am a software engineer currently studying Computer Science at Institut Teknologi Bandung. I specialize in backend development with experience in frontend development. I have a strong interest in security and computer architecture.
-                            </Typography>
-                            <Typography textAlign='left'
-                                variant={
-                                    breakpoint === 'xs' ? 'body2' :
-                                    breakpoint === 'sm' ? 'body2' :
-                                    breakpoint === 'md' ? 'body2' :
-                                    breakpoint === 'lg' ? 'body1' :
-                                    'body1'} mb={2}>
-                                        As a software engineer, I'm dedicated to building secure and efficient systems that meet high standards. With a detail-oriented mindset and strong analytical skills, I approach problems with excellence and take pride in developing effective solutions. My ultimate goal is to use my skills and knowledge to make a positive difference and bring innovative solutions to real-world problems.
-                            </Typography>
+                                    {slogan[0]}
+                                    <br/>
+                                    <span className="second-heading">{slogan[1]}</span>
+                                </Typography>
+                            </Box>
+                            <Box textAlign="left" color="white">
+                                <MuiMarkdown
+                                overrides={{
+                                    p: { component: Typography, props: { variant: ['xs', 'sm', 'md'].includes(breakpoint) ? 'body2' : 'body1', mb: 2 } },
+                                  }}>
+                                    {description}
+                                </MuiMarkdown>
+                            </Box>
                         </Grid>
                         <Grid item xs={12} sm={12} md={6} lg={4} order={{ xs: 1, sm: 1, md: 2 }} display='flex' style={{justifyContent: "center", alignItems: 'center'}} flexDirection="column">
                             <Box mt={{ xs: 4, sm: 4, md: 0 }} style={{position: "relative", display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: breakpoint === 'xs' ? '240px':'60vmin', maxHeight: breakpoint === 'xs' ? '240px':'60vmin', userSelect: "none" }}>
@@ -60,22 +91,22 @@ const About = () => {
                                 '& a': { mx: { xs: 0, sm: 1, md: 1, lg: 0, xl: 1 }, '&.active': {'& button': {color: '#ffffff', fontWeight: 'bold'}}},
                                 '& button': {transition: "0.3s", color: '#888888', '&:hover': {color: "#dddddd"}}
                             }}>
-                                <Link href="mailto:ahmadalfanihandoyo1@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <Link href={`mailto:${links.email}`} target="_blank" rel="noopener noreferrer">
                                     <Tooltip title="Email" arrow TransitionComponent={Zoom} componentsProps={{tooltip: { sx: { bgcolor: "#222222", '& .MuiTooltip-arrow': { color: "#222222"}}} }}>
                                         <IconButton> <Mail sx={{ fontSize: { xs: 32, sm: 42, md: 40, lg: 36, xl: 44 } }}/> </IconButton>
                                     </Tooltip>
                                 </Link>
-                                <Link href="https://www.linkedin.com/in/ahmad-alfani-handoyo" target="_blank" rel="noopener noreferrer">
+                                <Link href={links.linkedin} target="_blank" rel="noopener noreferrer">
                                     <Tooltip title="LinkedIn" arrow TransitionComponent={Zoom} componentsProps={{tooltip: { sx: { bgcolor: "#222222", '& .MuiTooltip-arrow': { color: "#222222"}}} }}>
                                         <IconButton> <LinkedIn sx={{ fontSize: { xs: 32, sm: 42, md: 40, lg: 36, xl: 44 } }}/> </IconButton>
                                     </Tooltip>
                                 </Link>
-                                <Link href="https://github.com/blueguy42" target="_blank" rel="noopener noreferrer">
+                                <Link href={links.github} target="_blank" rel="noopener noreferrer">
                                     <Tooltip title="GitHub" arrow TransitionComponent={Zoom} componentsProps={{tooltip: { sx: { bgcolor: "#222222", '& .MuiTooltip-arrow': { color: "#222222"}}} }}>
                                         <IconButton> <GitHub sx={{ fontSize: { xs: 32, sm: 42, md: 40, lg: 36, xl: 44 } }}/> </IconButton>
                                     </Tooltip>
                                 </Link>
-                                <Link href="https://www.instagram.com/afanhandoyo" target="_blank" rel="noopener noreferrer">
+                                <Link href={links.instagram} target="_blank" rel="noopener noreferrer">
                                     <Tooltip title="Instagram" arrow TransitionComponent={Zoom} componentsProps={{tooltip: { sx: { bgcolor: "#222222", '& .MuiTooltip-arrow': { color: "#222222"}}} }}>
                                         <IconButton> <Instagram sx={{ fontSize: { xs: 32, sm: 42, md: 40, lg: 36, xl: 44 } }}/> </IconButton>
                                     </Tooltip>
@@ -84,6 +115,8 @@ const About = () => {
                         </Grid>
                     </Grid>
                 </Grow>
+                </> :
+                <Loader/>}
             </motion.div>
         </>
     );
